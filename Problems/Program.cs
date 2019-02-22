540815c3b7dbbe551f607a9566753ab044fe06bd
sivesha <g16b0398@campus.ru.ac.za>
2019-02-22 16:16:45 +0200

    Added and committed Problems.Tests Files

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -2,11 +2,34 @@
 
 namespace Problems
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+        string Prefix(string input)
+        {
+            int length=0; int numWords=0;
+            if (input== null)
+            {
+                return ("0,0:");
+            }
+
+            string [] arrInput= input.Split();
+            length=arrInput.Length;
+            numWords= arrInput.Length;
+
+            foreach (string word in arrInput)
+            {
+                if (word==" ")
+                {
+                    numWords--;
+                }
+            }
+            return length+","+numWords+":"+input;
+
+
         }
     }
 }
