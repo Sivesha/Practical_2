acb539770ac1de5895d349d576435f426625d75d
sivesha <g16b0398@campus.ru.ac.za>
2019-02-23 13:19:25 +0200

    Update Unit tests

--- Problems.Tests/UnitTest1.cs
+++ Problems.Tests/UnitTest1.cs
@@ -16,16 +16,21 @@ namespace Tests
            Program p=new Program();
           string result= p.Prefix("hello");
           Assert.AreEqual("5,1:hello",result);
+
           string result2= p.Prefix("");
           Assert.AreEqual("0,0:",result2);
+
           string result3= p.Prefix("what ... did you say??");
           Assert.AreEqual("22,5:what ... did you say??",result3);
+
           string result4= p.Prefix("my name is");
-          Assert.AreEqual("10,3:my name is",result3);
+          Assert.AreEqual("10,3:my name is",result4);
+
           string result5= p.Prefix("see sharp..?!");
-          Assert.AreEqual("14,2:see sharp..?!",result3);
+          Assert.AreEqual("14,2:see sharp..?!",result5);
+
           string result6= p.Prefix(" hi ..Hello");
-          Assert.AreEqual("11,2: hi ..Hello",result3);
+          Assert.AreEqual("11,2: hi ..Hello",result6);
         
         }
         // public void Test2()
